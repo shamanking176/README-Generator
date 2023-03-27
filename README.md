a27412e919016deda52aba7320081058c448947f
Slug <adamloppe6@gmail.com>
2023-03-27 13:21:00 -0400

    added base code

---
+++ README.md
@@ -0,0 +1,24 @@
+
+  # Tets
+## Table of Content
+-[Project description](#Usage)
+-[Usage](#Usage)
+-[Contributing](#Contributing)
+-[Installation](#Installation)
+-[Questions](#Questions)
+-[License](#License)
+
+## Description
+Test
+## Usage
+Test
+## Installtion
+TEst
+## Contributiing
+undefined
+## Questions
+undefined
+undefined
+## License
+A
+
---
+++ index.js
@@ -0,0 +1,64 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const generateMarkdown = require('./lib/generatemarkdown');
+// TODO: Create an array of questions for user input
+const questions = [{
+    type:'input',
+    name:'title',
+    message:"Enter project title",
+},
+{
+    type:'input',
+    name:'description',
+    message:'Enter a description',
+},
+{
+    type:'input',
+    name:'installation',
+    message:'Enter installation instructions',
+},
+{
+    type:'input',
+    name:'usage',
+    message:'Enter usage information',
+},
+{
+    type:'input',
+    name:'contribution',
+    message:'Enter contribution guidelines',
+},
+{
+    type:'input',
+    name:'test instructions',
+    message:'Enter test instructions',
+},
+{
+    type:'list',
+    name:'license',
+    message:'Choose license',
+    choices:['A','B','C'],
+},]
+    ;
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, answers) {
+    return fs.writeFileSync(path.join(process.cwd(),fileName), answers)
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    return inquirer.prompt(questions)
+    .then((answers) =>{
+        writeToFile('README.md', generateMarkdown({...answers}))
+        
+
+})
+}
+
+
+
+
+// Function call to initialize app
+init();
